08661bd7d98ac18afc6783e4176b8e4ed3efb9e3
sodiasome <sodiasome@163.com>
2024-09-26 23:42:32 +0800

    fixed：在Game中添加一个player角色

--- AirPlane/Game.cpp
+++ AirPlane/Game.cpp
@@ -1,10 +1,15 @@
+﻿
 #include "Game.h"
-#include <SFML/Graphics.hpp>
 
-using namespace sf;
+
 
 Game::Game()
+	: mWindow(sf::VideoMode(640, 480), "AirPlane")
+	, mPlayer()
 {
+	mPlayer.setSize(sf::Vector2f(50.f,50.f));
+	mPlayer.setFillColor(sf::Color::Yellow);
+	mPlayer.setPosition(100.f,100.f);
 }
 
 Game::~Game()
@@ -13,19 +18,17 @@ Game::~Game()
 
 void Game::Run()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "AirPlane");
-	while (window.isOpen())
+	while (mWindow.isOpen())
 	{
 		sf::Event event;
-		while (window.pollEvent(event))
+		while (mWindow.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
-				window.close();
+				mWindow.close();
 		}
 
-		window.clear();
-		window.display();
-
+		//渲染
+		Render();	
 	}
 }
 
@@ -35,4 +38,7 @@ void Game::Update()
 
 void Game::Render()
 {
+	mWindow.clear();
+	mWindow.draw(mPlayer);
+	mWindow.display();
 }
--- AirPlane/Game.h
+++ AirPlane/Game.h
@@ -3,17 +3,21 @@
 // 游戏主逻辑
 // sodiasome@163.com
 
+#include <SFML/Graphics.hpp>
 
-
+using namespace sf;
 
 class Game
 {
 public:
 	Game();
 	~Game();
-	void Run();			//游戏主循环
+	void Run();					//游戏主循环
 private:
-	void Update();		//游戏操作更新
-	void Render();		//游戏渲染
+	void Update();				//游戏操作更新
+	void Render();				//游戏渲染
+private:
+	sf::RenderWindow mWindow;	//游戏窗口
+	sf::RectangleShape mPlayer;		//角色
 };
 
